c3d7d8e22067967b6f1a3f4fd0c1e156714d1db9
Vincibean <andrebessi00@gmail.com>
2018-12-16 14:28:36 +0000

    added create planes table SQL file

---
+++ flyway/src/main/resources/db/migration/V1__Create_planes_table.sql
@@ -0,0 +1,11 @@
+create table PLANES (
+    tailNum varchar(7) not null,
+    type varchar(20),
+    manufacturer varchar(30),
+    issueDate varchar(10),
+    model varchar(20),
+    status varchar(20),
+    aircraftType varchar(30),
+    engineType varchar(15),
+    constructionYear varchar(4)
+);
